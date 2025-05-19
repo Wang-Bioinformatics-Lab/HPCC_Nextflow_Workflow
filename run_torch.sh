121721eeb0c6b13b062ce4d98ba869559a28c13f
Mingxun Wang <mwang87@gmail.com>
2025-05-19 16:02:10 -0700

    updating torch

--- run_torch.sh
+++ run_torch.sh
@@ -1,9 +1,11 @@
-#!/bin/bash
+#!/bin/bash -l
+
 #SBATCH --job-name=gpu_job
 #SBATCH --partition=gpu
 #SBATCH --gres=gpu:1
 #SBATCH --cpus-per-task=8
 #SBATCH --mem=8G
 
-conda activate hello_world
-python ./test_torch.py
+export $PATH = '$CONDA_DEFAULT_ENV/bin:$PATH'
+source activate hello_world
+python ./bin/test_torch.py
